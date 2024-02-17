c6732f3656202b30b12db97d81a031fabe8a068d
harshlocked7 <harshlocked7@gmail.com>
2024-02-17 15:56:11 +0530

    Client side Frontend Structure

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import { Fragment } from 'react';
+import './App.css';
+
+//Components 
+import InputTodo from './components/InputTodo';
+import ListTodos from './components/ListTodo';
+function App() {
+  return <Fragment>
+    <div className='container'>
+    <InputTodo/>
+    <ListTodos/>
+      </div>
+    </Fragment>
+}
+
+export default App;
